be435f66c4f73cbaa423ae5de8d869ca26fb4653
William <wms274@nyu.edu>
2022-08-30 11:04:40 -0400

    commit tree e6f4e8e5c3c483f2c8fb96990d71a3701b19ce31

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = '';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new';
+}
